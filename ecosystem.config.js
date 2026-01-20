cd5b3e2bf72f8b94c5ed0d622cc28ae3718a883b
reishou <reishou90@gmail.com>
2026-01-20 23:59:47 +0700

    debug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,12 @@ module.exports = {
     {
       name: 'chomusuke-demo-next',
       script: 'pnpm',
-      args: 'dev -p 3001',
+      args: 'start -p 3001',
       cwd: '/var/www/chomusuke-demo-next',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        PORT: 3002,
-        ABC: Object.entries(process.env)
-        .map(([key, value]) => `${key}=${value}`)
-        .join('\n'),
-        DEF: "90000"
       },
       env_file: '.env.production',
     },
